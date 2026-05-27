2b49afe6c2977db09f052dcb790dffd614df0791
clementlr <clement.leroux@gmail.com>
2026-05-27 23:21:03 +0200

    adding first classes for baseline

---
+++ src/agent.py
@@ -0,0 +1,9 @@
+class Agent:
+    def __init__(self,name,model,api_key,temperature,role):
+        self.name = name
+        self.model = model
+        self.api_key = api_key
+        self.temperature = temperature
+        self.role = role
+    
+
