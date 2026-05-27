9437c2e35aaf543b3e3c81bba5232e11bcd13e3b
cl3m3nt <clement.leroux@gmail.com>
2026-05-27 23:27:09 +0200

    Cl3m3nt/feat/baseline (#1)
    * adding first classes for baseline

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from agent import Agent
+from user import User
+
+if __name__ == "__main__":
+
+    print("Getting started...")
+
+    u = User("test_user", "user")
+    print(u.__dict__)
+
+    a = Agent("test_agent", "model", "api_key", 0.5, "system")
+    print(a.__dict__)
