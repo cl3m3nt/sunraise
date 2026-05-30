2380bef930ee13a01778f9ce31fe2f43cb0b3851
cl3m3nt <clement.leroux@gmail.com>
2026-05-30 12:41:50 +0200

    adding user and agent identity (#8)

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,6 @@
-from google import genai
 import anthropic
+import uuid
+from google import genai
 from openai import OpenAI
 
 
@@ -10,6 +11,11 @@ class Agent:
         self.api_key = api_key
         self.temperature = temperature
         self.role = role
+        self.identity = self.get_identity()
+
+    def get_identity(self):
+        identity = uuid.uuid4()
+        return identity
 
     def call_dummy(self, message):
 
@@ -19,14 +25,12 @@ class Agent:
 
     def call_gemini(self, message):
         client = genai.Client(api_key=self.api_key)
-        print("calling gemini...")
         response = client.models.generate_content(model=self.model, contents=message)
 
         return response.text
 
     def call_claude(self, message):
         client = anthropic.Anthropic(api_key=self.api_key)
-        print("calling claude")
         prepared_message = {"role": "user", "content": message}
         response = client.messages.create(
             model=self.model, max_tokens=1000, messages=[prepared_message]
@@ -35,6 +39,5 @@ class Agent:
 
     def call_gpt(self, message):
         client = OpenAI(api_key=self.api_key)
-        print("calling gpt")
         response = client.responses.create(model=self.model, input=message)
         return response.output_text
--- src/user.py
+++ src/user.py
@@ -1,4 +1,12 @@
+import uuid
+
+
 class User:
     def __init__(self, name, role):
         self.name = name
         self.role = role
+        self.identity = self.get_identity()
+
+    def get_identity(self):
+        identity = uuid.uuid4()
+        return identity
