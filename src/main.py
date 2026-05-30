014e9834adb0692b97f2e2e0e78bcc0198433337
clementlr <clement.leroux@gmail.com>
2026-05-30 13:09:36 +0200

    adding mistral support for agent

--- src/main.py
+++ src/main.py
@@ -12,6 +12,8 @@ LLM_MODEL_CLAUDE = os.getenv("LLM_MODEL_CLAUDE")
 API_KEY_CLAUDE = os.getenv("API_KEY_CLAUDE")
 LLM_MODEL_GPT = os.getenv("LLM_MODEL_GPT")
 API_KEY_GPT = os.getenv("API_KEY_GPT")
+LLM_MODEL_MISTRAL = os.getenv("LLM_MODEL_MISTRAL")
+API_KEY_MISTRAL = os.getenv("API_KEY_MISTRAL")
 
 
 if __name__ == "__main__":
@@ -22,7 +24,7 @@ if __name__ == "__main__":
         "--provider",
         type=str,
         help="provider name",
-        choices=["dummy", "gemini", "claude", "gpt"],
+        choices=["dummy", "gemini", "claude", "gpt", "mistral"],
         default="dummy",
     )
     args = parser.parse_args()
@@ -38,6 +40,8 @@ if __name__ == "__main__":
         a = Agent("claude", LLM_MODEL_CLAUDE, API_KEY_CLAUDE, 0.5, "system")
     elif provider == "gpt":
         a = Agent("gpt", LLM_MODEL_GPT, API_KEY_GPT, 0.5, "system")
+    elif provider == "mistral":
+        a = Agent("mistral", LLM_MODEL_MISTRAL, API_KEY_MISTRAL, 0.5, "system")
     elif provider == "dummy":
         a = Agent("dummy", "llm_model", "api_key", 0.5, "system")
 
@@ -55,10 +59,12 @@ if __name__ == "__main__":
         else:
             if provider == "gemini":
                 agent_response = a.call_gemini(user_prompt)
-            if provider == "claude":
+            elif provider == "claude":
                 agent_response = a.call_claude(user_prompt)
-            if provider == "gpt":
+            elif provider == "gpt":
                 agent_response = a.call_gpt(user_prompt)
+            elif provider == "mistral":
+                agent_response = a.call_mistral(user_prompt)
             elif provider == "dummy":
                 agent_response = a.call_dummy(user_prompt)
             print(f"[agent]:{agent_response}")
