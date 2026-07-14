63c34fa749a98a0c795f79e2e2a73d619d8f0a49
clementlr <clement.leroux@gmail.com>
2026-07-14 16:35:54 +0200

    formating tools/read_skill.py

--- src/tools/read_skill.py
+++ src/tools/read_skill.py
@@ -1,13 +1,12 @@
 # get_weather tool func definition
-from dataclasses import dataclass
 from skills_loader import Skill, parse_skill, SKILLS_DIR
 
-    
-def read_skill(skill_name:str):
+
+def read_skill(skill_name: str):
     """Read a skill body from a given skill_name"""
-    
+
     skill_path = SKILLS_DIR + "/" + skill_name + "/" + "SKILL.md"
-    
+
     skill_metadata, skill_body, skill_msg = parse_skill(skill_path)
 
     if skill_metadata and skill_body:
@@ -18,7 +17,7 @@ def read_skill(skill_name:str):
             skill_path,
         )
 
-    return {"skill":skill_name,"body":s.body}
+    return {"skill": skill_name, "body": s.body}
 
 
 # get_weather tool config definition for google
