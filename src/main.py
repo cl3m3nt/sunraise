a0a81dc14c7b46705fbd02e5c53a0694681d831f
EmirhanFaruk <emirhansagir74@gmail.com>
2026-08-15 22:06:03 +0200

    Removing print statement

--- src/main.py
+++ src/main.py
@@ -288,7 +288,6 @@ if __name__ == "__main__":
             # ---------------------------------------------------------------------------
 
             user_prompt = input("[user]:")
-            print(f"User prompt: {user_prompt}")
             if provider == "anthropic":
                 user_message = {"role": "user", "content": user_prompt}
             elif provider == "dummy":
